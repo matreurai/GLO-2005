fb3bcc1226ebf47921ca57b5b6acb15f1bae0676
matreurai <sam_aubert@hotmail.com>
2021-04-12 15:50:47 -0400

    creation tables

--- Creation_Tables.sql
+++ Creation_Tables.sql
@@ -3,97 +3,102 @@ CREATE DATABASE `GLO-2005-Projet`;
 USE `GLO-2005`;
 -------------------------------------------------
 
-/* --Creation de la table Statistique-- */
-CREATE TABLE IF NOT EXISTS `t_statistique`
+/* --Creation de la table Projet-- */
+CREATE TABLE IF NOT EXISTS `t_projet`
 (
-    `symbole_statistique` VARCHAR(9) NOT NULL,
-    `statistique_capitalisation_boursiere` FLOAT,
-    `statistique_volume_max` BIGINT,
-    `statistique_quantite_circulation` BIGINT,
-    `statistique_volume_24h` BIGINT,
+    `projet_ticker` VARCHAR(9) NOT NULL,
+    `projet_logo` VARCHAR(50),
+    `projet_nom_du_coin` VARCHAR(20) NOT NULL,
+    `projet_secteur_activite` VARCHAR(20),
+    `projet_description` VARCHAR(300),
+    `projet_start_date` DATE,
+    `projet_forage_possible` BOOLEAN,
 
-    PRIMARY KEY(`symbole_statistique`)
+    PRIMARY KEY(`projet_ticker`)
 );
 
-ALTER TABLE `t_statistique` ENGINE InnoDB 
+ALTER TABLE `t_projet` ENGINE InnoDB
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
---CREATE INDEX `idx_statistique` ON `t_statistique`(`symbole_statistique`);
+CREATE INDEX `idx_projet` ON `t_projet`(`projet_ticker`);
 -------------------------------------------------------------------------------------------------------------------------
 
-/* --Creation de la table Information-- */
-CREATE TABLE IF NOT EXISTS `t_information`
+/* --Creation de la table Cryptomonnaie-- */
+CREATE TABLE IF NOT EXISTS `t_cryptomonnaie`
 (
-    `nom_information` VARCHAR(20) NOT NULL,
-    `information_symbole` VARCHAR(9) NOT NULL,
-    `information_secteur_activite` VARCHAR(20),
-    `information_date_entree_marche` DATE NOT NULL,
-    `information_description` VARCHAR(200),
-    `information_minee` ENUM('Oui','Non')  NOT NULL,
-   
-    PRIMARY KEY (`nom_information`),
-    FOREIGN KEY (`information_symbole`) REFERENCES `t_statistique`(`symbole_statistique`)
+    `cryptomonnaie_id` SMALLINT NOT NULL AUTO_INCREMENT,
+    `cryptomonnaie_ticker` VARCHAR(9) NOT NULL,
+    `cryptomonnaie_nom_du_coin` VARCHAR(20),
+    `cryptomonnaie_prix_actuel` FLOAT NOT NULL,
+    `cryptomonnaie_prix_haut` FLOAT,
+    `cryptomonnaie_prix_bas` FLOAT,
+    `cryptomonnaie_Valeur_cad` FLOAT,
+    `cryptomonnaie_market_cap` BIGINT,
+    `cryptomonnaie_max_supply` BIGINT,
+    `cryptomonnaie_qte_circulation` BIGINT,
+    `cryptomonnaie_volume_24h` BIGINT,
+    `cryptomonnaie_logo` VARCHAR(50),
+
+    PRIMARY KEY (`cryptomonnaie_id`),
+    FOREIGN KEY (`cryptomonnaie_ticker`) REFERENCES `t_projet`(`projet_ticker`)
 );
 
-ALTER TABLE `t_information` ENGINE InnoDB 
+ALTER TABLE `t_cryptomonnaie` ENGINE InnoDB
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
+CREATE INDEX `idx_projet` ON `t_cryptomonnaie`(`cryptomonnaie_id`);
 ------------------------------------------------------------------------------------------------------------------------
 
-/* --Creation de la table Portefeuille-- */
-CREATE TABLE IF NOT EXISTS `t_portefeuille`
+/* --Creation de la table Titre-- */
+CREATE TABLE IF NOT EXISTS `t_titre`
 (
-    `ID_portefeuille` SMALLINT NOT NULL AUTO_INCREMENT,
-    `portefeuille_symbole` VARCHAR(9) NOT NULL,
-    `portefeuille_quantite_possedee` INT,
-    `portefeuille_prix_achat` FLOAT,
-    `portefeuille_gains` SMALLINT,
-    `portefeuille_diff_achat_courant` FLOAT,
-   
-    PRIMARY KEY(`ID_portefeuille`),
-    FOREIGN KEY(`portefeuille_symbole`) REFERENCES `t_statistique`(`symbole_statistique`)
+    `titre_ticker` VARCHAR(9) NOT NULL,
+    `titre_qte` INT,
+    `titre_prix_paye` FLOAT,
+    `titre_valeur_courante` FLOAT,
+    `titre_profits` FLOAT,
+    `titre_ratio_%` FLOAT,
+
+    FOREIGN KEY(`titre_ticker`) REFERENCES `t_projet`(`projet_ticker`)
 );
 
-ALTER TABLE `t_portefeuille` ENGINE InnoDB 
+ALTER TABLE `t_titre` ENGINE InnoDB
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 ---------------------------------------------------------------------------------------------------------------------
 
-/* --Creation de la table Apercu-- */
-CREATE TABLE IF NOT EXISTS `t_apercu`
+/* --Creation de la table Portfolio-- */
+CREATE TABLE IF NOT EXISTS `t_portfolio`
 (
-    `ID_apercu` SMALLINT NOT NULL,
-    `apercu_symbole` VARCHAR(9) NOT NULL,
-    `apercu_nom` VARCHAR(20) NOT NULL,
-    `apercu_prix_reel` FLOAT NOT NULL,
-    `apercu_prix_haut` FLOAT NOT NULL,
-    `apercu_prix_bas` FLOAT NOT NULL,
-    `apercu_valeur_cad` FLOAT NOT NULL,
-    `apercu_valeur_us` FLOAT NOT NULL,
-   
-    FOREIGN KEY(`ID_apercu`) REFERENCES `t_portefeuille`(`ID_portefeuille`),
-    FOREIGN KEY(`apercu_symbole`) REFERENCES `t_statistique`(`symbole_statistique`),
-    FOREIGN KEY(`apercu_nom`) REFERENCES `t_information`(`nom_information`)
+    `portfolio_id` SMALLINT NOT NULL,
+    `portfolio_balance` FLOAT,
+    `portfolio_profit_total` FLOAT,
+    `portfolio_cout_total` FLOAT,
+    `portfolio_qte_coin_diff` SMALLINT,
+    `portfolio_ratio_%` FLOAT,
+
+    PRIMARY KEY(`portfolio_id`)
 );
 
-ALTER TABLE `t_apercu` ENGINE InnoDB 
+ALTER TABLE `t_portfolio` ENGINE InnoDB
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 -------------------------------------------------------------------------------------------------------------------------
 
-/* --Creation de la table Theme-- */
-CREATE TABLE IF NOT EXISTS `t_theme`
+/* --Creation de la table Alerte-- */
+CREATE TABLE IF NOT EXISTS `t_alerte`
 (
-    `ID_theme` TINYINT NOT NULL AUTO_INCREMENT,
-    `theme_nom` VARCHAR(25) NOT NULL,
-    `theme_police` VARCHAR(20) NOT NULL,
-    `theme_couleur_arriere_plan` VARCHAR(7) NOT NULL,
-    `theme_couleur_texte` VARCHAR(7) NOT NULL,
+    `alerte_id` TINYINT NOT NULL AUTO_INCREMENT,
+    `alerte_ticker` VARCHAR(25) NOT NULL,
+    `alerte_below_price` VARCHAR(20) NOT NULL,
+    `alerte_above_price` VARCHAR(7) NOT NULL,
+    `alerte_end_date` VARCHAR(7) NOT NULL,
 
-    PRIMARY KEY(`ID_theme`)
+    PRIMARY KEY(`alerte_id`),
+    FOREIGN KEY(`alerte_ticker`) REFERENCES `t_projet`(projet_ticker)
 );
 
-ALTER TABLE `t_theme` ENGINE InnoDB 
+ALTER TABLE `t_alerte` ENGINE InnoDB
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 -------------------------------------------------------------------------------------------------------------------------
@@ -101,32 +106,31 @@ COLLATE utf8mb4_unicode_ci;
 /* --Creation de la table Utilisateur-- */
 CREATE TABLE IF NOT EXISTS `t_utilisateur`
 (
-    `ID_utilisateur` SMALLINT NOT NULL AUTO_INCREMENT,
-    `utilisateur_nom_utilisateur` VARCHAR(20) NOT NULL,
-    `utilisateur_mdp` VARCHAR(20) NOT NULL,
-    `utilisateur_courriel` VARCHAR(40) NOT NULL,
-    `utilisateur_prenom` VARCHAR(30) NOT NULL,
-    `utilisateur_nom` VARCHAR(40) NOT NULL,
-    `utilisateur_age` TINYINT NOT NULL,
-    `utilisateur_sexe` ENUM('H', 'F', 'NB', 'NA') NOT NULL,
-    `utilisateur_theme` TINYINT DEFAULT 0,
-    `utilisateur_privilege` BOOLEAN DEFAULT 0,
+    `utilisateur_id` SMALLINT NOT NULL AUTO_INCREMENT,
+    `utilisateur_username` VARCHAR(20) NOT NULL,
+    `utilisateur_password` VARCHAR(20) NOT NULL,
+    `utilisateur_email` VARCHAR(40) NOT NULL,
+    `utilisateur_phone` VARCHAR(20),
+    `utilisateur_prenom` VARCHAR(30),
+    `utilisateur_nom` VARCHAR(40),
+    `utilisateur_age` TINYINT,
+    `utilisateur_sexe` VARCHAR(15),
     `utilisateur_date_creation` DATE NOT NULL,
 
-    PRIMARY KEY(`ID_utilisateur`),
-    FOREIGN KEY(`utilisateur_theme`) REFERENCES `t_theme`(`ID_theme`),
-    UNIQUE(`utilisateur_nom_utilisateur`),
-    UNIQUE(`utilisateur_courriel`)
+    PRIMARY KEY(`utilisateur_id`),
+    UNIQUE(`utilisateur_username`),
+    UNIQUE(`utilisateur_email`),
+    UNIQUE(`utilisateur_phone`)
 );
 
-ALTER TABLE `t_utilisateur` ENGINE InnoDB 
+ALTER TABLE `t_utilisateur` ENGINE InnoDB
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 ----------------------------------------------------------------------------------------------------------------------
 
-/* -------------------------------------------------------------------------------------------------------------------------- 
+/* --------------------------------------------------------------------------------------------------------------------------
                 -- EXEMPLE --
- --Peuplement de la table NOMDELATABLE-- 
+ --Peuplement de la table NOMDELATABLE--
 
 INSERT INTO `t_NOMDELATABLE`(attribut1, attribut2, attribut3, etc) VALUES
 (valeur1, valeur2, valeur3, etc),
@@ -159,14 +163,11 @@ INSERT INTO `t_apercu`(ID_apercu, apercu_symbole, apercu_nom, apercu_prix_reel, 
 (),
 ();
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-/* --Peuplement de la table Statistique-- */
-INSERT INTO `t_theme`(ID_theme, theme_nom, theme_police, theme_couleur_arriere_plan, theme_couleur_texte) VALUES
-(),
-();
+
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 /* --Peuplement de la table Statistique-- */
 INSERT INTO `t_utilisateur`(ID_utilisateur, utilisateur_nom_utilisateur, utilisateur_mdp, utilisateur_courriel, utilisateur_prenom, utilisateur_nom, utilisateur_age, utilisateur_sexe, utilisateur_theme, utilisateur_privilege, utilisateur_date_creation) VALUES
 (0, "matreurai", "123456", "saaub15@ulaval.ca", "Samuel", "Aubert", 27, 'H', 0, 1, now()),
-(1, "cactusman", "123456", "@ulaval.ca", "Christopher", "Godin", 27, 'H', 0, 1, now()),
-(2, "jarvis", "123456", "@ulaval.ca", "David", "Bolduc", 28, 'H', 0, 1, now());
+(1, "cactusman", "123456", "chgod54@ulaval.ca", "Christopher", "Godin", 22, 'H', 0, 1, now()),
+(2, "jarvis", "123456", "dabol66@ulaval.ca", "David", "Bolduc", 28, 'H', 0, 1, now());
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
