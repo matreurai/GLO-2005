64056eaec418c62a8619409d43940ba3e710b165
Jarvis_D.bolduc <gohabsgo2142@aim.com>
2021-04-12 19:15:24 -0400

    Test 1

---
+++ Code/API.py
@@ -0,0 +1,12 @@
+#-------------------------------
+# Test API Binance
+# David Bolduc
+#-------------------------------
+
+import json
+import requests
+
+
+x = 15
+result = x *2
+print (result)
---
+++ Code/json.js
@@ -0,0 +1,10 @@
+//-------------------------------
+//Test API Binance
+// David Bolduc
+//-------------------------------
+
+GET ("https://api.open-notify.org/this-api-doesnt-exist")
+print(response.status_code)
+
+
+
