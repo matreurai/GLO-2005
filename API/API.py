b5a43b027176e34a4a26d62bc7a98a72c5078620
matreurai <sam_aubert@hotmail.com>
2021-04-14 17:42:03 -0400

    ajout de l'API au main

---
+++ API/API.py
@@ -0,0 +1,45 @@
+#-------------------------------
+# Test API Binance
+# David Bolduc
+#-------------------------------
+
+import json
+import http.client
+from flask import Flask ,render_template , request
+import MySQLdb
+
+# Init App
+app = Flask(__name__)
+# Init Server
+db = MySQLdb.connect("localhost","root","christopher","glo-2005-projet")
+
+# Def du cursor
+cur = db.cursor()
+
+#Route de la page home
+@app.route('/')
+def main():
+  return render_template('Home.html')
+# time stamp
+
+def mise_a_jour():
+      return None
+
+if __name__ == '__main__':
+      app.run(debug=True)
+
+def exchange_dict ():
+  conn = http.client.HTTPSConnection("api.binance.com")
+  payload = ''
+  headers = {
+    'Content-Type': 'application/json'
+  }
+  conn.request("GET", "/api/v3/exchangeInfo", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+  transfor = data.decode("utf-8")
+  exchange_dict = json.loads(transfor)
+  print (type(exchange_dict['symbols']))
+  
+  for stocks in exchange_dict['symbols']:
+    return (stocks['symbol'])
