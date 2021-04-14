b7022938fce0ac1911ef77314f52506e54e0ca58
Jarvis_D.bolduc <gohabsgo2142@aim.com>
2021-04-14 11:58:42 -0400

    Autres test sql

--- API/API.py
+++ API/API.py
@@ -6,8 +6,25 @@
 import requests
 import json
 import http.client
+import API.connect_SQL
+from flask import Flask ,
+from flask_mysqldb import MySQL
 
+app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'GLO-2005-projet'
+
+mysql = MySQL(app)
+
+@approute('/')
+def index():
+      return render_template('home.html')
+
+if __name__ == '__main__':
+      app.run(debug=True)
 
 
 url = "https://api.binance.com/wapi/v3/systemStatus.html"
