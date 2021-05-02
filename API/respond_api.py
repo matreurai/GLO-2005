db7162194d9f20b396e00c8f5cb730114fd689da
gohabsgo2142 <gohabsgo2142@aim.com>
2021-05-02 11:49:54 -0400

    XLX,

---
+++ API/respond_api.py
@@ -0,0 +1,33 @@
+# ----------
+# Test respond Biannce
+# -------------
+
+import os
+import http.client
+import json
+
+
+# Coin market request
+# Sert a avoir les informations sur tous les coins.
+# ne dois pas etre utiliser souvent
+conn = http.client.HTTPSConnection("pro-api.coinmarketcap.com")
+payload = ''
+headers = {
+    'X-CMC_PRO_API_KEY': '618906da-2cdf-4e16-8356-3bd45841e5aa',
+    'Accept': 'application/json',
+    'Cookie': '__cfduid=dc40f08c990d9bb9621b3e61584f6e5951619128567'
+}
+conn.request("GET", "/v1/cryptocurrency/listings/latest?limit=20", payload, headers)
+res = conn.getresponse()
+data = res.read()
+respond = data.decode("utf-8")
+dict_data = json.loads(respond)['data']
+symbols = [v['symbol'] for v in dict_data if v['symbol']]
+names = [v['name'] for v in dict_data if v['name']]
+dates_added = [v['date_added'] for v in dict_data if v['date_added']]
+# print(symbol, name, date_added)
+
+
+def data():
+    symbol, name, date_added = symbols, names, dates_added
+    return symbol, name, date_added
